61c2c071d49d94fe63ebabf804db49cf0819b3a9
Sandip Gardi <sandipgardi935@gmail.com>
2023-07-25 23:08:37 +0530

    new config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,13 +3,25 @@ import nodemailer from 'nodemailer'
 const { AUTH_EMAIL, AUTH_PASS } = process.env
 // console.log(AUTH_EMAIL)
 
-let transporter = nodemailer.createTransport({
-  host: 'smtp-mail.outlook.com',
-  auth: {
-    user: AUTH_EMAIL,
-    pass: AUTH_PASS,
-  }
-})
+// let transporter = nodemailer.createTransport({
+//   host: 'smtp-mail.outlook.com',
+//   auth: {
+//     user: AUTH_EMAIL,
+//     pass: AUTH_PASS,
+//   }
+// })
+
+const transport = {
+  //this is the authentication for sending email.
+host: 'smtp.gmail.com',
+port: 465,
+secure: true, // use TLS
+
+auth: {
+  user: process.env.AUTH_EMAIL,
+  pass: process.env.AUTH_PASS,
+},
+}
 
 // test transporter
 transporter.verify((error, success) => {
