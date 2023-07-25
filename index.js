8b9ce223bde95e5513d450ca25cee1781ea06bac
Sandip Gardi <sandipgardi935@gmail.com>
2023-07-25 22:27:28 +0530

    name

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/otp', otpRoutes)
 
 
 app.get('/', (req, res) => {
-  res.send("This is a stack overflow clone's API by Gautam Jha")
+  res.send("This is a stack overflow clone's API by Sandip Gardi")
 })
 
 app.listen(PORT, () => {
